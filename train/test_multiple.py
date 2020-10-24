ee54af797c805b29225f399733f3268a023b3646
Fan Yang <yangfanthu16@gmail.com>
2020-10-24 18:26:17 +0800

    update model and test multiple

--- train/test_multiple.py
+++ train/test_multiple.py
@@ -21,13 +21,13 @@ if __name__ == "__main__":
     parser.add_argument("--start-timesteps", type=int, default=1e4)
     parser.add_argument("--max-timesteps", type=int, default=3e6)
     parser.add_argument("--eval-freq", type = int, default = 2000)
-    parser.add_argument("--broken-info", action='store_true', default=False,
+    parser.add_argument("--broken-info", action='store_true', default=True,
                         help="whether use broken joints indice as a part of state")
     parser.add_argument("--broken-info-recap", action='store_true', default=False,
                         help='whether to use broken info again in actor module to reinforce the learning')
     parser.add_argument("--save-freq", type=int, default=5000)
 
-    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--buffer-max-size", type=int, default=int(1e6))
     parser.add_argument("--agent-training-episodes", type=int, default=int(2))
     parser.add_argument("--adversary-training-episodes", type=int,default=int(1))
@@ -58,6 +58,7 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', type=int, default=256, metavar='N',
                         help='batch size (default: 256)')
     parser.add_argument('--trim-state', action="store_true", default=True)
+    parser.add_argument('--test-times', type=int, default=5)
     args = parser.parse_args()
     if args.broken_info_recap:
         assert args.broken_info
@@ -78,23 +79,15 @@ if __name__ == "__main__":
                 writer=None,
                 outdir=None,
                 device=device)
-    agent.restore_model_for_test(85000) 
-    # agent.restore_model_for_test(1100000) 
-    #1100000 works good
-    # 660000 works good
-    #trivial 225000 works perfect!!
-    current_state = env.reset()
+
+    # agent.restore_model_for_test(85000)  #for pure sac
+    agent.restore_model_for_test(1100000)  # for rarl sac
     # env.render()
-
-    if args.trim_state:
-        current_state = utils.trim_state(current_state)
-
-    broken_joints = [5] # 5 doesn't work
-
-    if args.broken_info:
-        current_state = np.concatenate((current_state, np.ones(9)))
-        for broken_one in broken_joints:
-            current_state[original_state_dim + broken_one] = 0
+    broken_scenarios=[]
+    for i in range(9):
+        for j in range(i + 1,9):
+            broken_scenarios.append([i,j])
+    
     previous_joint = np.zeros(9)
     sum_reward = 0
     index = 0
@@ -102,65 +95,84 @@ if __name__ == "__main__":
     env._max_episode_steps = 50
     broken_angle_1 = random.uniform(-0.8, 0.8)
     broken_angle_2 = random.uniform(-0.5, -1)
-    valve_angles = [0]
+    result_dict = {}
     with torch.no_grad():
-        while True:
-            if args.broken_info:
-                valve_angles.append(current_state[-10])
-            else:
-                valve_angles.append(current_state[-1])
-            "self diagnosed module"
-            # current_joint = current_state[:9]
-            # diff = current_joint - previous_joint
-            # diff = np.abs(diff)
-            # broken_loc = np.where(diff < 0.03)
-            # print(adversary_action)
-            for broken_one in broken_joints:
-                current_state[broken_one] = 0
-            action = agent.select_action(current_state, 'test')
-            index += 1
-            for broken_one in broken_joints:
-                if broken_one == 0 or broken_one == 3 or broken_one == 6:
-                    action[broken_one] = broken_angle_1
-                    # action[broken_one] = random.uniform(-1,1)
-                else:
-                    action[broken_one] = broken_angle_2
-                    # action[broken_one] = random.uniform(-0.5, -1)
-            # action[adversary_action[0]] = -0.6
-            # action[random.randint(0,8)] = 0
-            # joint = current_state[:9]
-            # cossin = current_state[9:11]
-            # command = current_state[11:20]
-            # other = current_state[20]
-            next_state, reward, done, info = env.step(action)
-            if args.trim_state:
-                next_state = utils.trim_state(next_state)
-            if args.broken_info:
-                next_state = np.concatenate((next_state, np.ones(9)))
-                for broken_one in broken_joints:
-                    current_state[original_state_dim + broken_one] = 0
-                    next_state[original_state_dim + broken_one] = 0
-            sum_reward += reward 
-            # print(sum_reward)
-            suc = info['score/success']
-            # previous_joint = current_joint
-            current_state = next_state
-            if done:
-                episode += 1
-                valve_angles = np.array(valve_angles)
-                np.save('./valve_angles_pure_5.npy', valve_angles)
+        for broken_joints in broken_scenarios:
+            success_record = []
+            for i in range(args.test_times):
+                env.seed(args.seed + 1)
+                env.action_space.seed(args.seed + 1)
+                torch.manual_seed(args.seed + 1)
+                np.random.seed(args.seed + 1)
+
+                success = False
+                done = False
                 current_state = env.reset()
-                previous_joint = np.zeros(9)
                 if args.trim_state:
                     current_state = utils.trim_state(current_state)
                 if args.broken_info:
                     current_state = np.concatenate((current_state, np.ones(9)))
-                
                     for broken_one in broken_joints:
                         current_state[original_state_dim + broken_one] = 0
-                print(sum_reward)
-                sum_reward = 0
-                
-                break
+                while True:
+                    "self diagnosed module"
+                    # current_joint = current_state[:9]
+                    # diff = current_joint - previous_joint
+                    # diff = np.abs(diff)
+                    # broken_loc = np.where(diff < 0.03)
+                    # print(adversary_action)
+                    for broken_one in broken_joints:
+                        current_state[broken_one] = 0
+                    action = agent.select_action(current_state, 'test')
+                    index += 1
+                    for broken_one in broken_joints:
+                        if broken_one == 0 or broken_one == 3 or broken_one == 6:
+                            action[broken_one] = broken_angle_1
+                            # action[broken_one] = random.uniform(-1,1)
+                        else:
+                            action[broken_one] = broken_angle_2
+                            # action[broken_one] = random.uniform(-0.5, -1)
+                    next_state, reward, done, info = env.step(action)
+                    if args.trim_state:
+                        next_state = utils.trim_state(next_state)
+                    if args.broken_info:
+                        next_state = np.concatenate((next_state, np.ones(9)))
+                        for broken_one in broken_joints:
+                            current_state[original_state_dim + broken_one] = 0
+                            next_state[original_state_dim + broken_one] = 0
+                    sum_reward += reward 
+                    # print(sum_reward)
+                    suc = info['score/success']
+                    # previous_joint = current_joint
+                    current_state = next_state
+                    if args.broken_info:
+                        valve_angle = abs(current_state[-10])
+                    else:
+                        valve_angle = abs(current_state[-1])
+                    if valve_angle < 0.3:
+                        done = True
+                        success = True
+                        
 
+                    if done:
+                        success_record.append(success)
+                        print(episode)
+                        episode += 1
+                        current_state = env.reset()
+                        previous_joint = np.zeros(9)
+                        if args.trim_state:
+                            current_state = utils.trim_state(current_state)
+                        if args.broken_info:
+                            current_state = np.concatenate((current_state, np.ones(9)))
+                        
+                            for broken_one in broken_joints:
+                                current_state[original_state_dim + broken_one] = 0
+                        # print(sum_reward)
+                        sum_reward = 0
+                        break
+            
+            success_rate = np.array(success_record).mean()
+            print("success_rate", success_rate)
+            result_dict[str(broken_joints)] = success_rate
+    np.save('./result.npy', result_dict, allow_pickle=True)
             
